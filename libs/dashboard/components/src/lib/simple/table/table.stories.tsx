3ff942e923832583a88738ee7c2755632d8c4036
Muzamil Shakir <mshakir39@gmail.com>
2022-11-26 16:03:30 +0500

    table story issue fixed

--- libs/dashboard/components/src/lib/simple/table/table.stories.tsx
+++ libs/dashboard/components/src/lib/simple/table/table.stories.tsx
@@ -1,9 +1,81 @@
+import { GridColDef } from '@mui/x-data-grid';
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Table from "./table";
 import { IReactTable } from "./table";
+
+
+
+const columns: GridColDef[] = [
+  {
+    field: "employeeName",
+    headerName: "Employee Name",
+    width: 130,
+    renderCell: (row) => (
+      <span title={row.row.employeeName}>{row.row.employeeName}</span>
+    ),
+  },
+  {
+    field: "role",
+    headerName: "Role",
+    width: 100,
+
+    renderCell: (row) => <span title={row.row.role}>{row.row.role}</span>,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 100,
+    renderCell: (row) => <span title={row.row.email}>{row.row.email}</span>,
+  },
+  {
+    field: "phoneNumber",
+    headerName: "Phone Number",
+    type: "number",
+    width: 130,
+    sortable: true,
+    renderCell: (row) => (
+      <span title={row.row.phoneNumber}>{row.row.phoneNumber}</span>
+    ),
+  },
+  {
+    field: "weeklyTargetBillingHours",
+    headerName: "Weekly Target Billing Hours",
+    sortable: true,
+    width: 160,
+  },
+  {
+    field: "vacationDayAllowance",
+    headerName: "Vacation Day Allowance",
+    sortable: true,
+    width: 160,
+  },
+  {
+    field: "sickDayAllowance",
+    headerName: "Sick Day Allowance",
+    sortable: true,
+    width: 160,
+  },
+  {
+    field: "billingRate",
+    headerName: "Billing Rate",
+    sortable: true,
+    width: 90,
+  },
+  {
+    field: "costRate",
+    headerName: "Cost Rate",
+    sortable: true,
+    width: 90,
+  },
+ 
+];
+
 export default {
   component: Table,
 } as ComponentMeta<typeof Table>;
+
+
+
 
 const Template: ComponentStory<typeof Table> = (args: IReactTable) => (
   <Table {...args} />
@@ -11,20 +83,10 @@ const Template: ComponentStory<typeof Table> = (args: IReactTable) => (
 
 export const Primary = Template.bind({});
 Primary.args = {
-  headers:[
-    "Employee Name",
-    "Role",
-    "Email",
-    "Phone Number",
-    "Weekly Target Billing Hours",
-    "Vacation Day Allowance",
-    "Sick Day Allowance",
-    "Billing Rate",
-    "Cost Rate",
-    " ",
-  ],
-  data: [
+  columns:columns,
+  tableData: [
     {
+      id:"0",
       employeeName: "Muzamil",
       role: "Manager",
       email: "mshakir39@gmail.com",
@@ -36,6 +98,7 @@ Primary.args = {
       costRate: "32",
     },
     {
+      id:"1",
       employeeName: "Muzasasamil",
       role: "Manager",
       email: "mshakir39@gmail.com",
